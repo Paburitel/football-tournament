1a95ceb25234015cbcd1c3355bbe772774fb5f57
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-06-30 17:41:16 +0300

    Update football-tournament.module.js

--- football/app/football-tournament.module.js
+++ football/app/football-tournament.module.js
@@ -1,8 +1,1 @@
-angular.module("footballTournament",["footballGroup" , "ngRoute"]).config([
-	"$routeProvider" , "$locationProvider" , function($routeProvider , $locationProvider){
-		$locationProvider.html5Mode(true);
-		$routeProvider.when("/group" , {
-			template: '<football-group data="teams" active="activeTeam" ng-if="visible"></football-group>'
-		})
-	}
-]);
+angular.module("footballTournament", ["footballGroup", "ngRoute"]);
