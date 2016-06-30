9d787ac58e7bf23295ad27ea44a16e7ec2a84339
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-06-30 17:41:37 +0300

    Update football-tournament.controller.js

--- football/app/football-tournament.controller.js
+++ football/app/football-tournament.controller.js
@@ -1,15 +1,8 @@
-
-function Controller() {
-	var $scope = arguments[0],
-	$http = arguments[1];
-
-	$http({
-		method: "GET",
-		url: "first-group.json"
-	}).then(function(response){
-		$scope.teams = response.data;
-		});
-}
-// Controller.$inject = ["$scope" , "$http"];
-angular.module("footballTournament").controller("footballTournament",
- ["$scope" , "$http" , Controller] );
+angular.module("footballTournament").controller("footballTournament", ["$scope", "$http", function($scope, $http){
+  $http({
+    method: "GET",
+    url: "first-group.json"
+  }).then(function(response){
+    $scope.teams = response.data;
+  });
+}]);
